fb5cd252b29fdce5faf78c9b97f982d00f34f270
jhonaumss <jhonaumss@gmail.com>
2025-06-01 16:40:59 -0400

    Test configuratoin

--- backend/src/test/java/com/taskmanagement/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/taskmanagement/backend/BackendApplicationTests.java
@@ -2,8 +2,14 @@ package com.taskmanagement.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class BackendApplicationTests {
 
 	@Test
