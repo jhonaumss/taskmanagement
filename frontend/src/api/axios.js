0919672785b83bdb2b689f87a5b178b3d57a94c8
jhonaumss <jhonaumss@gmail.com>
2025-06-01 23:10:23 -0400

    Improved env configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080',
   withCredentials: true,
 });
 
